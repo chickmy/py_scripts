c54e60c76301a09ac72aaa83cd501f53e16e45e1
chickmy <42132304+chickmy@users.noreply.github.com>
2020-07-17 04:21:06 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages  # noqa
 import os
 import sys
 
-# this_directory = os.path.abspath(os.path.dirname(__file__))
+## this_directory = os.path.abspath(os.path.dirname(__file__))
 # long_description = None
 # total_description = None
 # try:
